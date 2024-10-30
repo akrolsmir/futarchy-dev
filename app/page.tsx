194fe27b4c4be377fd68efb509b209a741025223
Nate Foss <npfoss@gmail.com>
2024-10-30 15:55:50 -0700

    title, subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,15 @@ import PredictionMarkets from './PredictionMarkets'
 export const revalidate = 60 // Revalidate every 60 seconds (1 minute)
 
 export default function Page() {
-  return <PredictionMarkets />
+  return (
+    <>
+      <h1 className="text-white text-4xl font-bold text-center px-4 py-8 mt-8">
+        How will the election affect you?
+      </h1>
+      <h2 className="text-slate-300 text-lg text-center px-4 pb-8">
+        Talk is cheap. Here's what people say when there's money on the line.
+      </h2>
+      <PredictionMarkets />
+    </>
+  )
 }
